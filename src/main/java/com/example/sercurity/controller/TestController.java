2aae2482460a3f315f6a7892bc0041667a47ac31
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-07-29 22:17:37 +0700

    security

---
+++ src/main/java/com/example/sercurity/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.example.sercurity.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+
+    @GetMapping("/all")
+    public String allAccess(){
+        return "Public Content";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN') ")
+    public String userAccess(){
+        return "User Access";
+    }
+
+
+
+}
